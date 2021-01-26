c8c9c3f49f03f58fbe148a7d33a59ab059e9ea75
Brad <bballpitcher@comcast.net>
2021-01-26 11:25:03 -0500

    new files

--- SchoolBot.py
+++ SchoolBot.py
@@ -5,14 +5,7 @@ from discord import Embed
 import asyncio
 
 
-def get_prefix(client, message):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    return prefixes[str(message.guild.id)]
-
-
-client = commands.Bot(command_prefix=get_prefix)
+client = commands.Bot(command_prefix='-')
 
 
 @client.event
